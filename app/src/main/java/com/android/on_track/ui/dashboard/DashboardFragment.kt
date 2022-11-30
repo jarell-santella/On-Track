723b3a620f4b9fd7292342da24663399759b6a92
js <50718474+jarell-santella@users.noreply.github.com>
2022-11-30 11:36:53 -0800

    dashboard with recyclerview and cardview functional

--- app/src/main/java/com/android/on_track/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/android/on_track/ui/dashboard/DashboardFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.on_track.data.Task
 import com.android.on_track.databinding.FragmentDashboardBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DashboardFragment : Fragment() {
 
@@ -17,21 +22,31 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // TODO: This needs to be used later on when Room database for tasks is implemented
+        val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val recyclerView: RecyclerView = binding.recyclerView
+        val addTaskButton: FloatingActionButton = binding.addTaskButton
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayout.VERTICAL, false)
+        // TODO: This will be from Room database for tasks instead
+        //  Need ViewModel for this class to get that from Room database which also needs to be made still
+        val taskList = ArrayList<Task>()
+        taskList.add(Task("Task Name 1", "Task Description 1", "0/1 months"))
+        taskList.add(Task("Task Name 2", "Task Description 2", "1/2 weeks"))
+        taskList.add(Task("Task Name 3", "Task Description 3", "2/7 days"))
+        taskList.add(Task("Task Name 4", "Task Description 4", "3/10 times"))
+
+        recyclerView.adapter = TaskAdapter(taskList)
+
+        addTaskButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Add Task button clicked", Toast.LENGTH_SHORT).show()
         }
+
         return root
     }
 
