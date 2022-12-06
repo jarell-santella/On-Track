a71975d1034e02c56d03773b3d6eb7bc3796098c
js <50718474+jarell-santella@users.noreply.github.com>
2022-12-06 15:53:35 -0800

    more sophisticated UI for recyclerview and viewholder

--- app/src/main/java/com/android/on_track/ui/dashboard/TaskAdapter.kt
+++ app/src/main/java/com/android/on_track/ui/dashboard/TaskAdapter.kt
@@ -2,20 +2,71 @@ package com.android.on_track.ui.dashboard
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.android.on_track.R
-import com.android.on_track.data.Task
+import com.android.on_track.data.tasks.Task
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TaskAdapter(private val taskList: ArrayList<Task>) : RecyclerView.Adapter<TaskViewHolder>() {
+class TaskAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
         return TaskViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.name.text = taskList[position].name.toString()
-        holder.description.text = taskList[position].description.toString()
-        holder.progress.text = taskList[position].progress.toString()
+        val name = taskList[position].name
+        val target = taskList[position].appName
+        val increment = taskList[position].increment
+        val progress = taskList[position].progress
+        val goalUnits = taskList[position].goalUnits
+        val goal = taskList[position].goal
+        val startDate = taskList[position].startDate
+        val durationUnits = taskList[position].durationUnits
+        val duration = taskList[position].duration
+
+        val timeDiff = Calendar.getInstance().time.time - startDate.time
+        val seconds = timeDiff / 1000
+        val minutes = seconds / 60
+        val hours = minutes / 60
+        val days = hours / 24
+        val weeks = days / 7
+
+        val timeRemaining = when (durationUnits) {
+            "Hours" -> duration - hours
+            "Days" -> duration - days
+            else -> duration - weeks // if durationUnits == "Weeks"
+        }
+        holder.name.text = name
+        holder.description.text = if (increment == "Positive") {
+            String.format("Use %s for %d %s for %d %s", target, goal, goalUnits.lowercase(), duration, durationUnits.lowercase())
+        } else { // if increment == "Negative"
+            String.format("Limit usage of %s for %d %s for %d %s", target, goal, goalUnits.lowercase(), duration, durationUnits.lowercase())
+        }
+        holder.progress.text = String.format("%d/%d %s", progress, goal, goalUnits.lowercase())
+        holder.startDate.text = String.format("Started %s", SimpleDateFormat("dd/MM/yyyy hh:mm").format(startDate))
+
+        if (progress >= goal) {
+            if (increment == "Positive") {
+                holder.timeRemaining.text = String.format("Success")
+                holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.context, R.color.pastel_green))
+            } else if (increment == "Negative") {
+                holder.timeRemaining.text = String.format("Failure")
+                holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.context, R.color.pastel_red))
+            }
+        } else if (timeRemaining >= 0) { // if progress < goal
+            holder.timeRemaining.text = String.format("%d %s remaining", timeRemaining, durationUnits.lowercase())
+            holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.context, R.color.pastel_yellow))
+        } else { // if progress < goal and timeRemaining < 0
+            if (increment == "Positive") {
+                holder.timeRemaining.text = String.format("Failure")
+                holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.context, R.color.pastel_red))
+            } else if (increment == "Negative") {
+                holder.timeRemaining.text = String.format("Success")
+                holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.context, R.color.pastel_green))
+            }
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/android/on_track/ui/dashboard/TaskViewHolder.kt
+++ app/src/main/java/com/android/on_track/ui/dashboard/TaskViewHolder.kt
@@ -3,10 +3,14 @@ package com.android.on_track.ui.dashboard
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.android.on_track.R
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
 
 class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val card: MaterialCardView = itemView.findViewById(R.id.task_card)
     val name: MaterialTextView = itemView.findViewById(R.id.task_name)
     val description: MaterialTextView = itemView.findViewById(R.id.task_description)
     val progress: MaterialTextView = itemView.findViewById(R.id.task_progress)
+    val startDate: MaterialTextView = itemView.findViewById(R.id.task_startDate)
+    val timeRemaining: MaterialTextView = itemView.findViewById(R.id.task_timeRemaining)
 }
